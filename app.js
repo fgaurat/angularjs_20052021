13fc50c420c9971696facf715d746abc3bd94db6
Frédéric Gaurat <fgaurat@eolem.com>
2021-05-20 15:24:53 +0200

    first

---
+++ app.js
@@ -0,0 +1,32 @@
+let todosApp = angular.module('todosApp',[])
+
+
+
+todosApp.controller('TodosController',function($scope){
+    $scope.title="Todolist App !"
+
+    const todos = [
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false,
+            "dueDate":1582620058000
+          },
+          {
+            "userId": 1,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false,
+            "dueDate":1582620058000
+          },
+          {
+            "userId": 1,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false,
+            "dueDate":1582620058000
+          },        
+    ]
+    $scope.todos = todos
+})  
